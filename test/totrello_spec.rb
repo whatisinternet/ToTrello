17de39a7275b6885b1e5435223900b904d0bd799
Whatisinternet <joshteeter@gmail.com>
2014-09-03 12:40:05 -0400

    Updated tests

--- test/totrello_spec.rb
+++ test/totrello_spec.rb
@@ -10,7 +10,7 @@ describe '#find_todo_items' do
   it 'returns a hash of todo items from a directory'do
     todo = ToDoFind.new
     todos = todo.search("#{Dir.pwd}/test/test_data")
-    expected = {:directory=>"test_data", :todo_list=>[{:file=>"/testing.rb", :todos=>[{:todo=>"test1", :location=>1}, {:todo=>"test2", :location=>2}, {:todo=>" test3\n", :location=>3}, {:todo=>": test4}\n", :location=>4}, {:todo=>"test5", :location=>5}, {:todo=>"test6", :location=>6}]}]}
+    expected = {:directory => "test_data", :todo_list => [{:file => "/testing.rb", :todos => [{:todo => "test1", :location => 1}, {:todo => "test2", :location => 2}, {:todo => "test3", :location => 3}, {:todo => "test4}", :location => 4}, {:todo => "test5", :location => 5}, {:todo => "test6", :location => 6}]}]}
     expect(todos).to include(expected)
   end
 
