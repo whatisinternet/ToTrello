22aaac97ac416954643338823d9d99e06294c2b2
Whatisinternet <joshteeter@gmail.com>
2014-09-05 15:07:34 -0400

    Code smell issues.. reduced obvious complexity

--- lib/to_do_find.rb
+++ lib/to_do_find.rb
@@ -29,41 +29,38 @@ class ToDoFind
     todos
   end
 
-  #TODO: Kind of important but this doesn't seem to be working...
-  private
   def find_todo(file)
+    todo_styles = ['TODO']
     @out = []
     code_lines = File.readlines(file)
+
     return @out unless code_lines.grep('/TODO/' || '/todo/')
 
-    line_num = 0
-    code_lines.each do |code_line|
-      line_num += 1
-      todo_location = is_todo?( code_line, 'TODO' )
-      todo_location ||= is_todo?( code_line, 'TODO:' )
-      todo_location ||= is_todo?( code_line, ' TODO:' )
-      todo_location ||= is_todo?( code_line, ' TODO' )
-      todo_location ||= is_todo?( code_line, '#TODO:' )
-      todo_location ||= is_todo?( code_line, '#TODO' )
-      unless todo_location.nil?
-        temp_string_array = code_line.split(' ')
-        todo = code_line.split(' ')[(todo_location + 1)..((temp_string_array.length) - 1)].join(' ')
-        #puts "Found! #{todo}"
-        todo_and_location = {:todo => todo,
-                             :location => line_num}
-        @out.append(todo_and_location)
-      end
+    code_lines= code_lines.map.with_index { |x,i| {:todo => x, :location => i + 1}}
 
-
-
+    todo_styles.each do |tds|
+      @out = code_lines.find_all { |i| i[:todo].include?(tds)}
     end
-    @out
+
+    clean_todos(@out)
+  end
+
+  private
+  def clean_todos(todo_array)
+    todo_array.each do |found_todos|
+      found_todos[:todo].gsub!('TODO:', '')
+      found_todos[:todo].gsub!('TODO', '')
+      found_todos[:todo].gsub!('#', '')
+      found_todos[:todo].chomp!
+      found_todos[:todo].lstrip!
+    end
+
   end
 
   private
   def is_todo?( string, test_string )
     begin
-      string.split(' ').index test_string
+      string.split(' ').index test_string != 0 ? true : false
     rescue
       return nil
     end
