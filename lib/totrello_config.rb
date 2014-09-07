f5030af364c3993c518fb58485e679c03672ab4e
Whatisinternet <joshteeter@gmail.com>
2014-09-06 23:56:40 -0400

    Fixed issue where if no .yml file was found nothing was returned causing crashes.

--- lib/totrello_config.rb
+++ lib/totrello_config.rb
@@ -2,20 +2,38 @@ require 'yaml'
 
 class TotrelloConfig
 
-  def read_config
-    config = YAML.load_file("totrello_config.yml")
+  def read_config(directory)
 
-    @project_name = config['totrello']['project_name']
-    @board_name   = config['totrello']['board']
-    @default_list = config['totrello']['list']
-    @excludes     = config['totrello']['exclude']
+    begin
+
+      rbfiles = File.join("#{Dir.pwd}/**", ".totrello.yml")
+      config = YAML.load_file( Dir.glob(rbfiles)[0])
+
+      puts 'Found a .totrello.yml file!'
+
+      project_name = config['totrello']['project_name']
+      board_name   = config['totrello']['board']
+      default_list = config['totrello']['list']
+      excludes     = Array(config['totrello']['exclude'])
+
+    rescue
+
+      puts 'No .yml file found or your yml file is corrupt. Making assumptions.'
+
+      project_name =  directory.split('/').last
+      board_name   = directory.split('/').last
+      default_list = 'To Do'
+      excludes     = Array(nil)
+
+    end
 
     {
-     :project_name => @project_name,
-     :board_name   => @board_name,
-     :default_list => @default_list,
-     :excludes     => @excludes
+     :project_name => project_name,
+     :board_name   => board_name,
+     :default_list => default_list,
+     :excludes     => excludes
     }
+
   end
 
 end
