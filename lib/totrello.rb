9a8daf61cfd4351cc626824ea6fda7d2ed9158aa
Whatisinternet <joshteeter@gmail.com>
2014-09-07 00:19:48 -0400

    Markdownified the descriptions

--- lib/totrello.rb
+++ lib/totrello.rb
@@ -93,10 +93,11 @@ module Totrello
     private
     def gen_description(file, todo, project_name)
       out =  "TODO item found by ToTrello\n"
-      out += "Project name #{project_name}\n"
-      out += "Filename: #{file}\n"
-      out += "Action item: #{todo[:todo]}\n"
-      out += "Location (at or near) line: #{todo[:location]}\n"
+      out +=  "===========================\n"
+      out += "**Project name:** #{project_name}\n"
+      out += "**Filename**: #{file}\n"
+      out += "**Action item**: #{todo[:todo]}\n"
+      out += "**Location (at or near) line**: #{todo[:location]}\n"
     end
 
 
