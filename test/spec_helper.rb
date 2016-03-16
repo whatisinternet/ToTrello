37d2510246862e878d9620cee239ff07efe770f8
Whatisinternet <joshteeter@gmail.com>
2016-03-16 15:04:58 -0400

    Removed old code since it is showing it's age.

--- test/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
-require 'totrello'
-require 'digest/sha1'
-
-TRELLO_DEVELOPER_PUBLIC_KEY ||= ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
-TRELLO_MEMBER_TOKEN ||= ENV['TRELLO_MEMBER_TOKEN']
