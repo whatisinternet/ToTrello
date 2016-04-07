8e61f19214a1059ef903e86189bb2027b0c6e9df
Whatisinternet <joshteeter@gmail.com>
2016-04-07 19:45:52 -0400

    Added global pry to rpsec and added config with options turned off.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,14 @@
 require "codeclimate-test-reporter"
+require 'pry'
 require 'dotenv'
 require 'vcr'
 require 'webmock/rspec'
-
+require 'bundler/setup'
 CodeClimate::TestReporter.start
+
+Bundler.setup
 Dotenv.load
+
 TRELLO_DEVELOPER_PUBLIC_KEY ||= ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
 TRELLO_MEMBER_TOKEN ||= ENV['TRELLO_MEMBER_TOKEN']
 
@@ -20,3 +24,8 @@ end
 
 # require 'totrello/totrello'
 require 'totrello'
+
+RSpec.configure do |c|
+  # c.profile_examples = true
+  # c.fail_fast = 1
+end
